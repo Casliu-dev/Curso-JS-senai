c7b071b44675ddf1bb89cba4ebb7daf36e0dedd4
Casliu-dev <pedroleitedev1@gmail.com>
2026-08-25 17:53:33 -0300

    refactor aula 5 comecando no js

---
+++ flexbox-playground/aula js/script.js
@@ -0,0 +1,41 @@
+//Linguagem interpretada, não precisa compilar, roda direto no navegador
+//node.js
+//para o JS funcionar, precisa colocar a tag <script> no final do body do HTML
+// , para que o HTML carregue primeiro e depois o JS. Se colocar no head
+//  o JS vai tentar acessar elementos do HTML que ainda não foram carregados, dando erro.
+//tipagem fraca
+
+//Condicionais (if, else, operadores lógicos,)
+
+
+
+//Exercício - Classificação da Temperatura
+//Pede uma temperatura em °C e diz se está muito frio, frio, agradável, quente ou muito quente
+
+//prompt() sempre devolve texto, então convertemos para número com Number()
+
+
+const temp = Number(prompt("qual a temperatura?"))
+
+if (number.isNaT(temp)) {
+    alert("temperatura invalida")
+} else if(temp <0) {
+
+}
+
+if (temp <0);
+    alert("muito frio");
+
+if (temp > 0 < 15);
+    alert ("Frio");
+
+if (temp > 16 < 15);
+    alert ("agradavel");
+
+if (temp > 26 < 35);
+    alert ("");
+ 
+if (temp > 35);
+    alert ("muito quente");
+
+    
